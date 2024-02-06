b31536eb6467a987c87c61dc5133f3e8c6c3ce0a
Juan <juansw03@gmail.com>
2024-02-06 13:53:50 -0500

    test: urlbox tests

--- src/components/Main/UrlBox.test.tsx
+++ src/components/Main/UrlBox.test.tsx
@@ -1,34 +1,49 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import UrlBox from './UrlBox'
 import '@testing-library/jest-dom'
 
-// Object.defineProperty(navigator, 'clipboard', {
-//   value: {
-//     writeText: jest.fn().mockImplementation(async () => { await Promise.resolve() })
-//   }
-// })
+const mockWriteText = jest.fn().mockImplementation(async () => { await Promise.resolve() })
+const originalClipboard = navigator.clipboard
+
+// @ts-expect-error clipboard readonly
+navigator.clipboard = { writeText: mockWriteText }
 
 describe('<UrlBox />', () => {
-  // let button: HTMLButtonElement
+  let button: HTMLButtonElement
 
   beforeEach(() => {
-    // render(<UrlBox oldLink="oldLink" newLink="newLink" />)
-    // button = screen.getByRole('button', { name: 'copyUrl' })
+    render(<UrlBox oldLink="oldLink" newLink="newLink" />)
+    button = screen.getByRole('button', { name: 'copyUrl' })
   })
 
   test('Should render correctly', () => {
-    render(<UrlBox oldLink="oldLink" newLink="newLink" />)
-
-    const button = screen.getByRole('button', { name: 'copyUrl' })
-
     expect(button).toBeInTheDocument()
   })
 
-  // describe('copy button', () => {
-  //   test('should change button class', () => {
-  //     fireEvent.click(button)
-  //     console.log(button.classList.contains('url-copy'))
-  //     expect(button.classList.contains('copied')).toBe(true)
-  //   })
-  // })
+  describe('copyThis function', () => {
+    afterEach(() => {
+      // @ts-expect-error clipboard readonly
+      navigator.clipboard = originalClipboard
+    })
+
+    test('copyThis function should execute navigator.clipboard', async () => {
+      fireEvent.click(button)
+
+      expect(mockWriteText).toHaveBeenCalledTimes(1)
+    })
+
+    // test('copyThis function should return early if button is not HTMLButtonElement', () => {
+    //   fireEvent.click((button as unknown) as HTMLDivElement)
+
+    //   expect(mockWriteText).toHaveBeenCalled()
+    // })
+
+    // test('copyThis function should return early if previous element is not HTMLAnchorElement', () => {
+    //   const fakeButton = document.createElement('div')
+
+    //   fireEvent.click(fakeButton)
+
+    //   expect(mockWriteText).not.toHaveBeenCalled()
+    // })
+  })
 })
